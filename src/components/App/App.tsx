ad3ff809801bb0d63b984fa5c8dd99caed5bdf3e
ibragim-ast <ibragim.ast@gmail.com>
2023-10-10 22:36:01 +0300

    feat(pagenotfound): создает компонент pageNotFound

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Recomended from '../Recomended/Recomended';
-import SearchResults from '../SearchResults/SearchResults';
+// import Recomended from '../Recomended/Recomended';
+// import SearchResults from '../SearchResults/SearchResults';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<SearchResults />
+			<NotFoundPage />
+			{/* <SearchResults />
 			<Recomended nearest={false} link="Все" title="Рекомендации" />
-			<Recomended nearest link="На карте" title="Ближайшие" />
-			<Footer />
+			<Recomended nearest link="На карте" title="Ближайшие" /> */}
+			{/* <Footer /> */}
 		</div>
 	);
 }
