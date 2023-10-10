1b021bb53a405ee81f4584416a452b39674cfae3
ibragim-ast <ibragim.ast@gmail.com>
2023-10-10 22:38:17 +0300

    fix(app): удаляет изменения в файле App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-// import Recomended from '../Recomended/Recomended';
-// import SearchResults from '../SearchResults/SearchResults';
+import Recomended from '../Recomended/Recomended';
+import SearchResults from '../SearchResults/SearchResults';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<NotFoundPage />
-			{/* <SearchResults />
+			<SearchResults />
 			<Recomended nearest={false} link="Все" title="Рекомендации" />
-			<Recomended nearest link="На карте" title="Ближайшие" /> */}
-			{/* <Footer /> */}
+			<Recomended nearest link="На карте" title="Ближайшие" />
+			<Footer />
 		</div>
 	);
 }
