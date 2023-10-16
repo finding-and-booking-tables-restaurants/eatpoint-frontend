5ef2b89a4f071ae3d291efbd1589f6b60fa020ec
ibragim-ast <ibragim.ast@gmail.com>
2023-10-16 20:56:34 +0300

    fix(profile): добавляет немного функционала

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Recomended from '../Recomended/Recomended';
-import SearchResults from '../SearchResults/SearchResults';
+// import Recomended from '../Recomended/Recomended';
+// import SearchResults from '../SearchResults/SearchResults';
+import Profile from '../Profile/Profile';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<SearchResults />
+			<Profile />
+			{/* <SearchResults />
 			<Recomended nearest={false} link="Все" title="Рекомендации" />
-			<Recomended nearest link="На карте" title="Ближайшие" />
+			<Recomended nearest link="На карте" title="Ближайшие" /> */}
 			<Footer />
 		</div>
 	);
