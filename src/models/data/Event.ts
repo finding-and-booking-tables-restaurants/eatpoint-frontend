ee68de7c9d657f1806f3ef335c557de3f9d42efa
Nikita <117741649+Augenb1ick@users.noreply.github.com>
2023-12-12 13:20:40 +0400

    add: event cards

---
+++ src/models/data/Event.ts
@@ -0,0 +1,10 @@
+export interface Event {
+	name: string;
+	establishment: string;
+	description: string;
+	date_start: string;
+	date_end: string;
+	type_event: number[];
+	price: number;
+	image: string;
+}
